3d87a81bf30115801201af0c989e163d923ad00a
shiyuyi <syuyi_1994@163.com>
2018-05-17 16:40:14 +0800

    添加icon

--- src/main/java/com/example/demo/dao/jdbc/StudeneJdbcDao.java
+++ src/main/java/com/example/demo/dao/jdbc/StudeneJdbcDao.java
@@ -1,22 +1,12 @@
 package com.example.demo.dao.jdbc;
-import com.example.demo.domain.Student;
-import com.sun.xml.internal.fastinfoset.util.PrefixArray;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.spi.MetadataImplementor;
-import org.hibernate.boot.spi.SessionFactoryOptions;
-import org.hibernate.internal.SessionFactoryImpl;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
-import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
+
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.Resource;
-import javax.crypto.MacSpi;
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.Map;
